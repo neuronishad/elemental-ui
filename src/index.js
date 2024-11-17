dff534ef1be83cdfa1e2a1faae87cf8165b6294c
John Doe <john@doe.tld>
2024-11-17 16:33:54 +0530

    added container, button, form, basic inputs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './main.css';
 
 // Components
 import './container/container';
+import './button/button';
+import './form/form';
+import './input/input';
+import './input/textarea';
+import './input/select';
