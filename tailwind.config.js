1b9670bae5bd81a2747ae06112cb258e82f1cd8e
John Doe <john@doe.tld>
2024-11-17 16:28:14 +0530

    start

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,js}', // Look for classes in these files
+    ],
+    theme: {
+        extend: {
+            colors: {
+                // primary: '#1E3A8A', // Custom primary color
+            },
+        },
+    },
+    plugins: [],
+};
+
